adc73ee2ed57673e9f149cbfb3015daa6bef1f0a
Destiny81 <Destiny040713@gmail.com>
2020-10-18 09:31:36 -0700

    message

---
+++ src/WhileLoopPractice.java
@@ -0,0 +1,9 @@
+public class WhileLoopPractice {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 5); { // loop start
+            System.out.println("PeopleNTect"); // loop intialization
+            x++; // loop ends
+        }
+    }
+}
